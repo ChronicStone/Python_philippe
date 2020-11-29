35af4b740897da1bc5dce6131c0ec2f63cc58721
philippe macy <cyprienthao@gmail.com>
2020-11-29 19:49:08 +0100

    test

---
+++ le_wagon_exercises/ex1 hello world!/math_for_dummies.py
@@ -0,0 +1,9 @@
+# pylint: disable=missing-docstring,invalid-name
+
+def summify(x, y):
+    return x+y
+    pass
+
+def multiply(x, y):
+    return x*y
+    pass
---
+++ le_wagon_exercises/ex3_financials/financials 2.0.py
@@ -0,0 +1,15 @@
+# pylint: disable=missing-docstring
+
+# TODO: 1st exercise: Define the `forward_price` function
+def forward_price(spot, interest_rate, time):
+    return round(spot * 2.7182 ** (interest_rate * time), 2)
+
+
+
+# TODO: 2nd exercise: Define the `short_pnl` funct
+
+def short_pnl(positions, mtm): 
+    value = 0
+    for (index, key) in enumerate(positions):
+        value += (positions[index] - mtm[index])
+    return value
